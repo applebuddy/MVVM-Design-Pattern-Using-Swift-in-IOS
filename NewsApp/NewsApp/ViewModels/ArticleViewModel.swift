d9355e9462a617687c147f698cc0be981f3179d2
applebuddy <eagermin92@gmail.com>
2022-10-04 23:02:38 +0900

    Study lecture 10. Implementing View Models

---
+++ NewsApp/NewsApp/ViewModels/ArticleViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  ArticleViewModel.swift
+//  NewsApp
+//
+//  Created by MinKyeongTae on 2022/10/04.
+//
+
+import Foundation
+
+struct ArticleListViewModel {
+  private let articles: [Article]
+}
+
+extension ArticleListViewModel {
+  var numberOfSections: Int {
+    return 1
+  }
+  
+  func numberOfRowsInSection(_ section: Int) -> Int {
+    return self.articles.count
+  }
+  
+  func articleAtIndex(_ index: Int) -> ArticleViewModel {
+    return ArticleViewModel(self.articles[index])
+  }
+}
+
+struct ArticleViewModel {
+  private let article: Article
+}
+
+extension ArticleViewModel {
+  init(_ article: Article) {
+    self.article = article
+  }
+}
+
+extension ArticleViewModel {
+  
+  var title: String {
+    return self.article.title
+  }
+  
+  var description: String {
+    return self.article.description
+  }
+}
