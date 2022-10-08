08a0670f188985a9f21de682831e142f3c055105
applebuddy <eagermin92@gmail.com>
2022-10-08 19:38:10 +0900

    Study lecture 19. HotCoffee App, Creating Models

---
+++ HotCoffee/HotCoffee/Models/Order.swift
@@ -0,0 +1,30 @@
+//
+//  Order.swift
+//  HotCoffee
+//
+//  Created by MinKyeongTae on 2022/10/08.
+//
+
+// MARK:  19. Let's implement Creating Models!!
+
+import Foundation
+
+enum CoffeeType: String, Codable {
+  case cappuccino
+  case latte
+  case espressino
+  case corado
+}
+
+enum CoffeeSize: String, Codable {
+  case small
+  case medium
+  case large
+}
+
+struct Order: Codable {
+  let name: String
+  let email: String
+  let type: CoffeeType
+  let size: CoffeeSize
+}
