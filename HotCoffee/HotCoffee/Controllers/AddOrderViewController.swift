ecf96aaa39d0eefee2e473783cd3f04f725352f8
applebuddy <eagermin92@gmail.com>
2022-10-08 23:28:35 +0900

    Study lecture 25. HotCoffee App, Adding Checkmark for Selected Cells
    we can set checkmark or none as accessoryType when selected, deselected state of tableView

--- HotCoffee/HotCoffee/Controllers/AddOrderViewController.swift
+++ HotCoffee/HotCoffee/Controllers/AddOrderViewController.swift
@@ -33,6 +33,14 @@ extension AddOrderViewController: UITableViewDelegate, UITableViewDataSource {
     return 1
   }
   
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+  }
+  
+  func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+    tableView.cellForRow(at: indexPath)?.accessoryType = .none
+  }
+  
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return self.viewModel.types.count
   }
