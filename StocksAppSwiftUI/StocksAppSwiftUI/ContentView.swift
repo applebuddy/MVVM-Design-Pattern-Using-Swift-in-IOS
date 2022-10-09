d64da9c1aef9b387e726095edc8401fab4bdfd71
applebuddy <eagermin92@gmail.com>
2022-10-09 20:03:11 +0900

    Study lecture 61. StocksApp(SwiftUI, MVVM), Setting Up UserInterface in SwiftUI

--- StocksAppSwiftUI/StocksAppSwiftUI/ContentView.swift
+++ StocksAppSwiftUI/StocksAppSwiftUI/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+  
+  @State private var searchTerm: String = ""
+  
+  init() {
+    UINavigationBar.appearance().backgroundColor = .black
+    UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+    UITableView.appearance().backgroundColor = .black
+    UITableViewCell.appearance().backgroundColor = .black
+  }
+  
+  var body: some View {
+    NavigationView {
+      ZStack(alignment: .leading) {
+        Color.black
+        VStack(alignment: .leading) {
+          Text("January 5 2020")
+            .font(.custom("Arial", size: 32))
+            .fontWeight(.bold)
+            .foregroundColor(Color.gray)
+            .padding(EdgeInsets(top: 10, leading: 20, bottom: 0, trailing: 0))
+          
+          // SearchView의 @Binding 변수에 바인딩 변수 전달을 위해 @State 변수 이름 앞에 '$'를 붙여 전달한다.
+          SearchView(searchTerm: $searchTerm)
+          
+          Spacer()
+        }
+      }
+      .navigationBarTitle("Stocks")
+    }.edgesIgnoringSafeArea(Edge.Set(.bottom))
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
---
+++ StocksAppSwiftUI/StocksAppSwiftUI/Views/SearchView.swift
@@ -0,0 +1,31 @@
+//
+//  SearchView.swift
+//  StocksAppSwiftUI
+//
+//  Created by MinKyeongTae on 2022/10/09.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+  @Binding var searchTerm: String
+  
+  var body: some View {
+    HStack {
+      Image(systemName: "magnifyingglass")
+      TextField("Search", text: self.$searchTerm)
+      Spacer()
+    }
+    .frame(height: 50)
+    .foregroundColor(.secondary)
+    .background(Color(.secondarySystemBackground))
+    .cornerRadius(10)
+    .padding(10)
+  }
+}
+
+struct SearchView_Previews: PreviewProvider {
+  static var previews: some View {
+    SearchView(searchTerm: .constant(""))
+  }
+}
