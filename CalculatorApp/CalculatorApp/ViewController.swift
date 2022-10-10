19ee1523347061702386746ed8173002f3ecba59
applebuddy <eagermin92@gmail.com>
2022-10-10 20:02:02 +0900

    Study lecture 74. Setting Up Your Project to do unit test usinig XCTest

---
+++ CalculatorApp/CalculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by MinKyeongTae on 2022/10/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ CalculatorApp/CalculatorAppTests/CalculatorAppTests.swift
@@ -0,0 +1,14 @@
+//
+//  CalculatorAppTests.swift
+//  CalculatorAppTests
+//
+//  Created by MinKyeongTae on 2022/10/10.
+//
+
+// MARK: 74. Settinig Up Your Project
+// Tests 파일에서 테스트코드를 작성하여 Unit Test를 할 수 있다.
+import XCTest
+
+class CalculatorAppTests: XCTestCase {
+
+}
