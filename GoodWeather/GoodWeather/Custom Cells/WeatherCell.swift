abdc5b96f6b909888374670b503f50294103f527
applebuddy <eagermin92@gmail.com>
2022-10-17 19:44:10 +0900

    Study lecture 37. WeatherApp(UIKit+MVVM), Displaying Hard-Coded Weather into TableView

---
+++ GoodWeather/GoodWeather/Custom Cells/WeatherCell.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherCell.swift
+//  GoodWeather
+//
+//  Created by MinKyeongTae on 2022/10/17.
+//
+
+import UIKit
+
+class WeatherCell: UITableViewCell {
+  
+  @IBOutlet weak var cityNameLabel: UILabel!
+  @IBOutlet weak var temperatureLabel: UILabel!
+}
