fcf32994df2013d7f1d622ebad346f2438638e64
applebuddy <eagermin92@gmail.com>
2022-10-17 23:28:22 +0900

    Study lecture 49. WeatherApp(UIKit+MVVM), Displaying Temperature Units on Settings Screen

--- GoodWeather/GoodWeather/Controller/AppSettingTableViewController.swift
+++ GoodWeather/GoodWeather/Controller/AppSettingTableViewController.swift
@@ -9,4 +9,25 @@ import UIKit
 
 class AppSettingTableViewController: UITableViewController {
   
+  private var settingsViewModel = SettingsViewModel()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.navigationController?.navigationBar.prefersLargeTitles = true
+  }
+  
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return settingsViewModel.units.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let settingsItem = settingsViewModel.units[indexPath.row]
+    let cell = tableView.dequeueReusableCell(withIdentifier: "SettiingsCell", for: indexPath)
+    cell.textLabel?.text = settingsItem.displayName
+    return cell
+  }
 }
