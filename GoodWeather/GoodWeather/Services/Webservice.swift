a0a2915fc3a79fbff78b3bfc0e61597347e53bda
applebuddy <eagermin92@gmail.com>
2022-10-17 20:30:44 +0900

    Study lecture 39 ~ 40. WeatherApp(UIKit+MVVM), Implementing Weabservice class

---
+++ GoodWeather/GoodWeather/Services/Webservice.swift
@@ -0,0 +1,30 @@
+//
+//  Webservice.swift
+//  GoodWeather
+//
+//  Created by MinKyeongTae on 2022/10/17.
+//
+
+import Foundation
+
+struct Resource<T: Codable> {
+  let url: URL
+  let parse: (Data) -> T?
+}
+
+final class Webservice {
+  func load<T>(resource: Resource<T>, completion: @escaping (T?) -> Void) {
+    URLSession.shared.dataTask(with: resource.url) { data, response, error in
+      guard let data = data else {
+        DispatchQueue.main.async {
+          completion(nil)
+        }
+        return
+      }
+      DispatchQueue.main.async {
+        completion(resource.parse(data))
+      }
+    }
+    .resume()
+  }
+}
