afa6984a18ee24dd213cb24f80f9d1f85c8165f0
applebuddy <eagermin92@gmail.com>
2022-10-17 22:27:01 +0900

    Study lecture Section 8. Ready to implement AppSettingTableViewController with ViewModel

---
+++ GoodWeather/GoodWeather/Controller/AppSettingTableViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AppSettingTableViewController.swift
+//  GoodWeather
+//
+//  Created by MinKyeongTae on 2022/10/17.
+//
+
+import UIKit
+
+class AppSettingTableViewController: UITableViewController {
+  
+}
