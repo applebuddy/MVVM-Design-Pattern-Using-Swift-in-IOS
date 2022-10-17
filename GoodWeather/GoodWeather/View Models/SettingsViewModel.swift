2174a596806e7a02c37478810a9d11522c389754
applebuddy <eagermin92@gmail.com>
2022-10-17 22:35:17 +0900

    Study lecture 48. WeatherApp(UIKit+MVVM), Implementing SettingsViewModel for AppSettingTableViewController

---
+++ GoodWeather/GoodWeather/View Models/SettingsViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  SettingsViewModel.swift
+//  GoodWeather
+//
+//  Created by MinKyeongTae on 2022/10/17.
+//
+
+import Foundation
+
+enum Unit: String, CaseIterable {
+  case celsius = "metric"
+  case fahrenheit = "imperial"
+}
+
+extension Unit {
+  var displayName: String {
+    get {
+      switch(self) {
+      case .celsius:
+        return "Celcius"
+      case .fahrenheit:
+        return "Fahrenheit"
+      }
+    }
+  }
+}
+
+class SettingsViewModel {
+  let units = Unit.allCases
+  var selectedUnit: Unit {
+    get {
+      let userDefaults = UserDefaults.standard
+      var unitValue = ""
+      if let value = userDefaults.value(forKey: "unit") as? String {
+        unitValue = value
+      }
+      return Unit(rawValue: unitValue)!
+    }
+    set {
+      let userDefaults = UserDefaults.standard
+      userDefaults.set(newValue, forKey: "unit")
+    }
+  }
+}
